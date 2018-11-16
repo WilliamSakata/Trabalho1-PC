35332fee9bd06134b98f510d0b5ad4a2177220ea
williamsakata <williamsakata2@gmail.com>
2018-11-16 11:16:09 -0200

    Leitura do arquivo funcionando

--- main.c
+++ main.c
@@ -5,7 +5,48 @@
 float **alocmat(int lin, int col);
 
 int main(){
-  
+  int nVertices, i, j;
+  FILE *arq;
+  char nomeArq[20];
+  float **matriz;
+  float w; //w=weight
+
+  printf("Digite o nome do arquivo a ser aberto: ");
+  scanf("%s", nomeArq);
+
+  arq=fopen(nomeArq, "r");
+
+  if(arq==NULL){
+    printf("\nErro ao abrir o arquivo!!\n");
+    exit(1);
+  }else{
+    printf("Digite a quantidade de vértices do grafo: ");
+    scanf("%d", &nVertices);
+
+    matriz = alocmat(nVertices, nVertices);
+    printf("\npassei alocmat\n");
+
+    printf("\ni=%d, j=%d, w=%.2f\n", i,j,w);
+
+    while(fscanf(arq, "%d %d %f", &i, &j, &w) != EOF){
+      printf("\ni=%d, j=%d, w=%.2f\n", i,j,w);
+      matriz[i-1][j-1]=w;
+    }
+    printf("\npassei while\n");
+
+    printf("\n");
+
+    printf("%f\n", matriz[499][499]);
+
+    for(i=0;i<nVertices;i++){
+      for(j=0;j<nVertices;j++){
+        printf("%.2f ", matriz[i][j]);
+      }
+      printf("\n");
+    }
+
+  }
+  free(matriz);
 }
 
 float **alocmat(int lin, int col){
